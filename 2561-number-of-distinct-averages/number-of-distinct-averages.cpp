9359bc3112d654c042b46997729caa96679e77ee
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-27 19:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (17.57%) - LeetSync

---
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int l=0,r=n-1;
+        unordered_set<double>s;
+        while(l<r){
+            double avg = (nums[l]+nums[r]) / 2.0;
+            s.insert(avg);
+            l++,r--;
+
+        }
+        return s.size();
+    }
+};
