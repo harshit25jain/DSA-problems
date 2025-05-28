58a1ceed7cc23700f3b8e252e3add19a64ebe4c0
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-28 13:43:16 +0530

    Time: 8 ms (47.22%) | Memory: 13.3 MB (25.51%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,33 @@
+
+        class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        // Create DP table
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        
+        // Initialize first row and column
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i; // Delete all characters from word1
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j; // Insert all characters from word2
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1]; // No operation needed
+                } else {
+                    // Minimum of insert, delete, replace
+                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
