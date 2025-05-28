35c762bbbefcd365f40409a54d9f0dc6e2ffb681
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-28 22:05:18 +0530

    Time: 83 ms (46.09%) | Memory: 174.1 MB (18.73%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+//TWO POINTER APPROACH
+    //Functin to expand arounf center and return longest palindrome
+    string expandAroundCenter(string s,int left, int right){
+        while(left>=0 && right<s.size() && s[left] == s[right]){
+            left--,right++;
+        }
+        return s.substr(left+1,right-left-1);
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if(n == 0) return "";
+
+        string longest = "";
+
+        for(int i=0;i<n;i++){
+            //odd length palindrome(single character)
+            string odd = expandAroundCenter(s,i,i);
+            if(odd.length() > longest.length()){
+                longest = odd;
+            }
+
+            //even length palindrome (two char center)
+            string even = expandAroundCenter(s,i,i+1);
+             if(even.length() > longest.length()){
+                longest = even;
+            }
+        }
+        return longest;
+    }
+};
