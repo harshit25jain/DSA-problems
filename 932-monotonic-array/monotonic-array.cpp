3bb2b1ca27ae0320434bc3e1a8893df10cc5078c
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-29 17:56:07 +0530

    Time: 4 ms (22.86%) | Memory: 100.4 MB (64.37%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        //one directional flag
+         int n = nums.size();
+        if (n <= 2) return true;
+
+        int direction = 0; // 0 = not decided, 1 = increasing, -1 = decreasing
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) continue;
+            int currDir = (nums[i] > nums[i - 1]) ? 1 : -1;
+            if (direction == 0) {
+                direction = currDir;
+            } else if (direction != currDir) {
+                return false;
+            }
+        }
+        return true;
+
+
+    /*
+    class Solution {
+public: //brute force approach
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true, decreasing = true;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) increasing = false;
+            if (nums[i] > nums[i - 1]) decreasing = false;
+        }
+        return increasing || decreasing;
+    }
+};
+
+    */   
+    }
+};
