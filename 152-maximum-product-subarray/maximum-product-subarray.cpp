1f42f7b48c870cfb6f1e9fd98201d61cf696e53a
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-29 15:36:44 +0530

    Update maximum-product-subarray.cpp

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -21,3 +21,17 @@ public:
     return result;
     }
 };
+//brute force approach
+int maxProduct(vector<int>& nums) {
+    int n = nums.size(), result = INT_MIN;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            int prod = 1;
+            for (int k = i; k <= j; ++k) {
+                prod *= nums[k];
+            }
+            result = max(result, prod);
+        }
+    }
+    return result;
+}
