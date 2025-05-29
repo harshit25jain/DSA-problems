d2b2c99e88ca593947ba9103a6deb87f458929da
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-29 18:13:27 +0530

    Update rotate-string.cpp

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -5,3 +5,13 @@ public:
         return s.length() == goal.length() && (s + s).find(goal) != string::npos;
     }
 };
+/*brute force approach  bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // rotate left by 1: s = s.substr(1) + s[0]
+            s = s.substr(1) + s[0];
+            if (s == goal) return true;
+        }
+        return false;
+    } O(n^2) n for rotations × O(n) for each substr()*/
