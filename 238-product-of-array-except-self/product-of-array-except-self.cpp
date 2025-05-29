6de629fcdc45c126db35307f53a387879685676c
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-29 17:30:43 +0530

    Update product-of-array-except-self.cpp

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//prefix+suffix approach TC:O(N),SC:O(1)
     vector<int> productExceptSelf(vector<int>& nums) {
     int n = nums.size();
     vector<int> res(n, 1);
@@ -20,3 +21,19 @@ public:
         
     }
 };
+/* // TC:O(N) SC: O(N)-> prefix+suffix array
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> prefix(n, 1), suffix(n, 1), result(n);
+
+    for (int i = 1; i < n; i++)
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+
+    for (int i = n - 2; i >= 0; i--)
+        suffix[i] = suffix[i + 1] * nums[i + 1];
+
+    for (int i = 0; i < n; i++)
+        result[i] = prefix[i] * suffix[i];
+
+    return result;
+}*/
