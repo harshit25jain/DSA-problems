4fa2633421f244043bd17b7729997f76b54cb502
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-30 22:48:42 +0530

    Update intersection-of-two-arrays-ii.cpp

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-    //the idea is to use hashmap
+    //the idea is to use hashmap TC: O(n + m) SC:O(N)
     unordered_map<int, int> freq;
     for(int num : nums1) {
         freq[num]++;
@@ -17,3 +17,26 @@ public:
     return result;
 }
 };
+/*(Sort + Two Pointers) TC:O(n log n + m log m) SC: O(1) (excluding result)
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+
+    int i = 0, j = 0;
+    vector<int> result;
+
+    while(i < nums1.size() && j < nums2.size()) {
+        if(nums1[i] == nums2[j]) {
+            result.push_back(nums1[i]);
+            i++; j++;
+        }
+        else if(nums1[i] < nums2[j]) {
+            i++;
+        }
+        else {
+            j++;
+        }
+    }
+
+    return result;
+}*/
