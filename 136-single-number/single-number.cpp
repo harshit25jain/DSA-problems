8f577cc7d30b17e713cc7f7eab7b9d27aa1e9eed
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-05-30 22:22:27 +0530

    Update single-number.cpp

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -8,4 +8,15 @@ public:
     }
     return result;
 }
-};
+};/*The better approach woule be to use hashmapint 
+singleNumber(vector<int>& nums) {
+    unordered_map<int, int> freq;
+    for(int num : nums) {
+        freq[num]++;
+    }
+    for(auto [key, value] : freq) {
+        if(value == 1) return key;
+    }
+    return -1;
+}
+*/
