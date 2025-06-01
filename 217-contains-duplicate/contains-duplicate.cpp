6ea730fec752aa47d92f61ec0e86a5d97f91b09e
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-01 15:15:59 +0530

    Time: 53 ms (39.63%) | Memory: 90.8 MB (36.34%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int>seen;//hashset to store unique numbers
+
+        for(int num : nums){ //iterating through the array nums
+            if(seen.find(num) != seen.end()){
+                //check if already present
+                return true;
+            }
+            seen.insert(num); //insert into set
+        }
+
+    return false;    
+    }
+};
