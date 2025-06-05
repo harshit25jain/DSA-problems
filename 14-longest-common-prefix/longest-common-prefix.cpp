d25a1d8daaaa6298bcbe790b56ea4dea0a33d0bd
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-05 22:31:08 +0530

    Update longest-common-prefix.cpp

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -6,7 +6,7 @@ public:
         string prefix = strs[0]; //assume the first string as prefix
 
         for(int i=1;i<strs.size();i++){
-            //reduce the prefix tomatch the current string
+            //reduce the prefix to match the current string
             while(strs[i].find(prefix) !=0){
                 prefix= prefix.substr(0,prefix.size()-1);
                 if(prefix.empty()) return "";
