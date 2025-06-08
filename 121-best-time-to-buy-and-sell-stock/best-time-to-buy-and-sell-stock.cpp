ba70bd8b7778a027c9d59da0eaea84de1780e7bc
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-08 21:10:52 +0530

    Update best-time-to-buy-and-sell-stock.cpp

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-
 //OPTIMAL: O(N) -> Track the min price seen so far
     int maxProfit(vector<int>& prices) {
         int minPrice = INT_MAX; 
         int maxProfit = 0;
-
         for(int price: prices){
             if(price < minPrice){
                 minPrice = price; //update min price if find a lower price
