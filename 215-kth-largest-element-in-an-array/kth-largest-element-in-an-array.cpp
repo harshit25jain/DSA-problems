5801ee59090caa58fbbb995ecf3cc75a77b9d325
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-10 20:39:11 +0530

    Update kth-largest-element-in-an-array.cpp

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) { //TC:O(NLogk)
-         priority_queue<int, vector<int>, greater<int>> minHeap;
-    
+        
+    priority_queue<int, vector<int>, greater<int>> minHeap;
     for (int num : nums) {
         minHeap.push(num);            // Add element
         if (minHeap.size() > k) {
             minHeap.pop();            // Keep heap size = k
         }
     }
-    
     return minHeap.top();             // kth largest
     }
 };
+
 /*  BRUTE FORCE APPROACH TC:O(nlogn)
 int findKthLargest(vector<int>& nums, int k) {
     sort(nums.begin(), nums.end());  // Sort in ascending order
