db852e2dbc29698d8448f29e96852ea62e03bbb3
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-19 23:20:16 +0530

    Update 3sum-closest.cpp

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-//sORTING + 2POINTER APPROACH
+
+//SORTING + 2POINTER APPROACH
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int closestSum = nums[0] + nums[1] + nums[2];
         int n = nums.size();
-        for(int i=0;i<n-2;i++){
-            int left=i+1;
-            int right=nums.size()-1;
+        for(int i=0; i<n-2; i++){
+            int left = i+1;
+            int right = nums.size()-1;
 
-            while(left<right){
+            while(left < right){
                 int currentSum = nums[i] + nums[left] + nums[right];
 
                 if(abs(target - currentSum) < abs(target-closestSum)){
@@ -22,7 +23,6 @@ public:
                 }else{
                     return currentSum;
                 }
-
             }
         }
      return closestSum;   
