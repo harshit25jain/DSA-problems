c72ce16631400e2573962c5c59614435a5693a46
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-20 22:51:01 +0530

    Update contains-duplicate.cpp

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        unordered_set<int>seen;//hashset to store unique numbers
+        unordered_set<int>seen; //hashset to store unique numbers
 
         for(int num : nums){ //iterating through the array nums
             if(seen.find(num) != seen.end()){
