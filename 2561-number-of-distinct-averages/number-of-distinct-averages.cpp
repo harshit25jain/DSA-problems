8c36489bd493aa0bece7d10765f977fce5746aa1
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-21 00:23:29 +0530

    Update number-of-distinct-averages.cpp

--- 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
@@ -5,7 +5,7 @@ public:
         int n=nums.size();
         int l=0,r=n-1;
         unordered_set<double>s;
-        while(l<r){
+        while(l < r){
             double avg = (nums[l]+nums[r]) / 2.0;
             s.insert(avg);
             l++,r--;
