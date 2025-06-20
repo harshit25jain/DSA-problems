f0b9d2b386ab5fb07f57ad7de4ee37d553620e92
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-20 23:26:59 +0530

    Update valid-anagram.cpp

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -11,11 +11,10 @@ public:
             count[s[i] - 'a']++;
             count[t[i] - 'a']--;
         }
-        for(int i=0;i<26;i++){
-            if(count[i] !=0 )
+        for(int i=0; i<26; i++){
+            if(count[i] != 0 )
             return false;
         }
-        return true;
-        
+        return true;      
     }
 };
