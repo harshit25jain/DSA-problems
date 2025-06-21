be19e64d58cf0e9b9776eeec1f557374d38cd723
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-21 22:20:19 +0530

    Update count-pairs-whose-sum-is-less-than-target.cpp

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        int n=nums.size();
+        
+        int n = nums.size();
         sort(nums.begin(),nums.end());
-        int left = 0,right =n-1;
+        int left = 0,right = n-1;
         int count=0;
         while(left<right){
            if(nums[left] + nums[right] < target){
