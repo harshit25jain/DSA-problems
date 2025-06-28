e466f1711d1ef2bc0d004b1476c090c95a1a7020
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-28 15:44:57 +0530

    Update longest-substring-without-repeating-characters.cpp

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -16,3 +16,13 @@ public:
         
     }
 };
+/*
+I’m using the sliding window approach with a hashmap to track the last seen index of each character. 
+As I move the right pointer through the string:
+
+If the character is already in the hashmap and is inside the current window (i.e., its index is ≥ left), 
+I move the left pointer just after its previous position — to remove the duplicate.
+Then I update the hashmap with the latest index of the character.
+
+I calculate the current window size as right - left + 1, and update the maxLength if this window is bigger than the previous one.
+*/
