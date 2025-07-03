25279310851b136833f991f4f8a0a22d726b659d
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-07-03 23:42:55 +0530

    Update majority-element.cpp

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = -1, count=0;
+        int candidate = -1, count = 0;
 
         for(int num: nums){
-            if(count==0){
-                candidate=num; //assign new candidate
+            if(count == 0){
+                candidate = num; //assign new candidate
             }
             count += (num==candidate) ? 1 : -1;
         }
