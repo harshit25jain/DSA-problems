e24ddae6ed18b966a9844873ce7f25c1fb5f0352
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-11-26 15:07:09 +0530

    Refactor peakIndexInMountainArray for clarity

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int start = 1,end =arr.size() - 2;
-        while(start<=end) {
+        int start = 1, end = arr.size() - 2;
+        while(start <= end) {
             int mid = start + (end - start)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
             return mid;
 
-            else if(arr[mid]>arr[mid-1]){
+            else if(arr[mid] > arr[mid-1]){
                 start = mid+1;
             }
             else{
