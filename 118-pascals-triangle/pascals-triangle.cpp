4488d0cc277d19318d54322e458e190a8b33f632
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-12-13 11:51:19 +0530

    Update pascals-triangle.cpp

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -15,3 +15,38 @@ public:
         
     }
 };
+
+
+/*C# Code 
+using System.Collections.Generic;
+
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        IList<IList<int>> triangle = new List<IList<int>>();
+
+        for (int i = 0; i < numRows; i++)
+        {
+            List<int> row = new List<int>();
+
+            // Initialize row with 1s
+            for (int j = 0; j <= i; j++)
+            {
+                row.Add(1);
+            }
+
+            // Compute middle values
+            for (int j = 1; j < i; j++)
+            {
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+
+            triangle.Add(row);
+        }
+
+        return triangle;
+    }
+}
+
+*/
