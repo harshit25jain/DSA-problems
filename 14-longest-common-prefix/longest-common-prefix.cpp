7f063d55e23a252fe20b9e4f37afd6606ee6ead7
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-12-15 07:49:19 +0530

    Update longest-common-prefix.cpp

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -15,3 +15,33 @@ public:
         return prefix;
     }
 };
+
+/* C# Code 
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
+            return "";
+
+        string prefix = strs[0]; // assume the first string as prefix
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            // reduce the prefix to match the current string
+            while (!strs[i].StartsWith(prefix))
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix.Length == 0)
+                    return "";
+            }
+        }
+
+        return prefix;
+    }
+}
+
+
+
+
+*/
