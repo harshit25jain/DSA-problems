a937b641372cfcdade5fafe53dfae46682593b57
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-12-17 18:26:38 +0530

    Update container-with-most-water.cpp

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -19,35 +19,3 @@ public:
     }
 };
 
-
-/*C# Code
-
-using System;
-
-public class Solution
-{
-    public int MaxArea(int[] height)
-    {
-        int left = 0, right = height.Length - 1;
-        int maxWater = 0;
-
-        while (left < right)
-        {
-            int h = Math.Min(height[left], height[right]);
-            int w = right - left;
-            maxWater = Math.Max(maxWater, h * w);
-
-            if (height[left] < height[right])
-            {
-                left++;
-            }
-            else
-            {
-                right--;
-            }
-        }
-
-        return maxWater;
-    }
-}
-*/
