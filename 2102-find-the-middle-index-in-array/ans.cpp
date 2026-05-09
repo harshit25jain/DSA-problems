d0040091b6090c287bd6938a2192d530c1ebd9af
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2026-05-09 18:47:04 +0530

    Create ans.cpp

---
+++ 2102-find-the-middle-index-in-array/ans.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int totalSum = 0;
+
+        // Step 1: Find total sum of array
+        for(int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        // Step 2: Traverse array
+        for(int i = 0; i < nums.size(); i++) {
+
+            // Right sum = total sum - left sum - current element
+            int rightSum = totalSum - leftSum - nums[i];
+
+            // Check if left sum and right sum are equal
+            if(leftSum == rightSum) {
+                return i;
+            }
+
+            // Update left sum
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
