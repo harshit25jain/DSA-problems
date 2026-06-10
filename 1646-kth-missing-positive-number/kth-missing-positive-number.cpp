b9c7725039fde536485ba73b706b2043b6f4b3a9
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2026-06-10 12:11:49 +0530

    Add binary search implementation for Kth missing positive
    Implemented binary search approach to find the Kth missing positive number.

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//APPROACH1: Binary search TC: O(LOGN)
     int findKthPositive(vector<int>& arr, int k) {
         int start = 0, end = arr.size()-1, mid,
         ans = arr.size();
@@ -16,3 +17,23 @@ public:
     return ans + k;
     }
 };
+/*
+APPROACH2: Linear Scan TC: O(N)
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+
+        for(int i=0;i<arr.size();i++){
+
+            int missing = arr[i] - (i+1);
+
+            if(missing >= k)
+                return i + k;
+
+        }
+
+        return arr.size() + k;
+    }
+};
+
+*/
