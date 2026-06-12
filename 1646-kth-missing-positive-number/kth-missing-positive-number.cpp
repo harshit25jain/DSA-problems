39651234bee4a66e23d342d3bcb028fde1683a6b
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2026-06-12 21:40:33 +0530

    Update kth-missing-positive-number.cpp

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -18,6 +18,16 @@ public:
     }
 };
 /*
+"The key observation is that the number of missing positive integers 
+before index i can be calculated using arr[i] - i - 1. Since this missing 
+count increases monotonically, I can apply Binary Search to find the 
+first index where the missing count becomes at least k. 
+If the missing count at mid is large enough, I store that index 
+and search left for an earlier occurrence; otherwise, I move right. 
+After finding the boundary index, the kth missing positive number can be computed directly as ans + k.
+*/
+
+/*
 APPROACH2: Linear Scan TC: O(N)
 class Solution {
 public:
