f418411e23bee3182ab213a53ce75845857b4200
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2026-06-12 17:09:44 +0530

    Update longest-common-prefix.cpp

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -15,6 +15,19 @@ public:
         return prefix;
     }
 };
+/*
+"The key idea is to start with the first string as a candidate prefix because any
+common prefix must be part of the first string. Then, for every other string, 
+I verify whether the current prefix appears at the beginning of that string. 
+If it doesn't, I keep shortening the prefix by removing one character at a time 
+until it becomes a valid prefix. By continuously reducing invalid prefixes, 
+I eventually obtain the longest prefix common to all strings. 
+If the prefix becomes empty, it means no common prefix exists.
+
+
+*/
+
+
 
 /* C# Code 
 public class Solution
