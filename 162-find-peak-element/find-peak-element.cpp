4fee42eec37081728ff3cf1b7df5259b4b586665
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2026-06-12 19:49:51 +0530

    Update find-peak-element.cpp

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -17,9 +17,11 @@ public:
 };
 /*
 "I used Binary Search because I don't need the maximum element; I only need any peak element. 
-The key observation is that by comparing nums[mid] and nums[mid+1], I can determine the direction of the slope.
+The key observation is that by comparing nums[mid] and nums[mid+1], 
+I can determine the direction of the slope.
 If nums[mid] > nums[mid+1], the slope is decreasing. 
-This means we are either standing on a peak or have already crossed one, so a peak must exist at mid or somewhere on the left side.
+This means we are either standing on a peak or have 
+already crossed one, so a peak must exist at mid or somewhere on the left side.
 Therefore, I keep the left half by setting right = mid.
 If nums[mid] < nums[mid+1], the slope is increasing.
 This means a higher value exists on the right, so we haven't reached a peak yet.
